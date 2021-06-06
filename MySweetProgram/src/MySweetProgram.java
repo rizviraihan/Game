ef7a6bdf02a2c138dbb2d82682eebd9389605e37
Rizvi Raihan <rizvi.raihan@bjitgroup.com>
2021-06-07 01:19:12 +0900

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name");
+
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		
+		System.out.println(s);
+
+
+		in.close();
+	}
+}
