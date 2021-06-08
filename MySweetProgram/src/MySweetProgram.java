9f59b6e6a76d0e2487a7ef43c9ed10b7dd61f02c
Rizvi Raihan <rizvi.raihan@bjitgroup.com>
2021-06-08 11:14:56 +0900

    iterators

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,21 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		LinkedList<String> names = new LinkedList<String>();
 		names.push("Rizvi");
 		names.push("Sanzida");
 		names.push("Waniya");
 
-		System.out.println(names.pop());
-		System.out.println(names.removeFirst());
-		System.out.println(names.remove());
+		ListIterator<String> it = names.listIterator();
+		it.next();
+		it.next();
+		it.add("susan");
+
+		for (String s : names) {
+			System.out.println(s);
+		}
 	}
 }
